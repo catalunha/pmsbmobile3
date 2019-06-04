5f68af3b8dd9e04d5ade0b74326ebb21ba3bf894
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-06-04 19:34:17 -0300

    wip: suporte para login google inicial na classe UserRepository https://github.com/catalunha/pmsbmobile3/issues/9

--- lib/state/user_repository.dart
+++ lib/state/user_repository.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 enum Status { Uninitialized, Authenticated, Authenticating, Unauthenticated }
 
 class UserRepository with ChangeNotifier {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
   FirebaseAuth _auth;
   FirebaseUser _user;
   Status _status = Status.Uninitialized;
@@ -26,6 +28,29 @@ class UserRepository with ChangeNotifier {
       notifyListeners();
       return false;
     }
+  }
+
+  Future<bool> googleSignIn() async{
+    print("tentando logar: ");
+    try{
+      _status = Status.Authenticating;
+      notifyListeners();
+      final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      await _auth.signInWithCredential(credential);
+      return true;
+    }catch(e){
+      print(e);
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      return false;
+    }
+
+
   }
 
   Future signOut() async {
