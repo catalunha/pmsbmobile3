b9a0482c623e12cc2b1e9679895ae6081336a36b
paulorobertocruz <paulo.cruz9@gmail.com>
2019-06-12 19:32:34 -0300

    adiciona tela para listagem de noticias arquivadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pmsbmibile3/pages/noticias/noticias_page.dart';
 import 'package:provider/provider.dart';
 import 'package:pmsbmibile3/state/user_repository.dart';
 import 'package:pmsbmibile3/pages/home.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         initialRoute: "/",
         routes: {
           "/": (context) => HomePage(),
+          "/noticias/noticias_visualizadas": (context) => NoticiasVisualizadasPage(),
         },
       ),
     );
