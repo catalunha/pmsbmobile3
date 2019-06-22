f7778fb594938ac0fc89025f3b8396f20ec9cf9e
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-06-22 18:02:42 -0300

    implemantação representação do documento perfis_usuarios

--- lib/models/perfis_usuarios_model.dart
+++ lib/models/perfis_usuarios_model.dart
@@ -1,4 +1,26 @@
 class PerfilUsuarioModel{
+  String id; //mesmo id do usuario
   String nome;
+  String email;
+  String celular;
+  String setorCensitario;
+  String nomeProjeto;
+  String imagemPeril;
 
+  dynamic cargo;
+  List<dynamic> eixos;
+  dynamic eixo_atual;
+  dynamic eixo;
+
+  List<String> rotas;
+
+  PerfilUsuarioModel.fromMap(Map<String, dynamic> map) {
+    id = map['id'] ?? "";
+    nome = map['nome'] ?? "";
+    email = map['email'] ?? "";
+    celular = map['celular'] ?? "";
+    setorCensitario = map['setorCensitario'] ?? "";
+    nomeProjeto = map['nomeProjeto'] ?? "";
+    imagemPeril = map['imagemPerfil'] ?? "";
+  }
 }
