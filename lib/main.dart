a81f9d90e57021d6c5d661eded463cbaf956d0a1
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-06-23 02:55:22 -0300

    transmite bloc auth para toda a arvore do app

--- lib/main.dart
+++ lib/main.dart
@@ -4,50 +4,54 @@ import 'package:provider/provider.dart';
 import 'package:pmsbmibile3/state/user_repository.dart';
 import 'package:pmsbmibile3/pages/pages.dart';
 import 'package:pmsbmibile3/state/services.dart';
+import 'package:pmsbmibile3/state/auth_bloc.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (_) => UserRepository.instance(),
-      child: Provider<DatabaseService>.value(
-        value: DatabaseService(),
-        child: MaterialApp(
-          title: 'PMSB',
-          theme: ThemeData.dark(),
-          initialRoute: "/",
-          routes: {
-            "/": (context) => HomePage(),
+    return Provider.value(
+      value: AuthBloc(),
+      child: ChangeNotifierProvider(
+        builder: (_) => UserRepository.instance(),
+        child: Provider<DatabaseService>.value(
+          value: DatabaseService(),
+          child: MaterialApp(
+            title: 'PMSB',
+            theme: ThemeData.dark(),
+            initialRoute: "/",
+            routes: {
+              "/": (context) => HomePage(),
 
-            //perfil
-            "/perfil": (context) => PerfilPage(),
-            "/perfil/editar_variavel": (context) => PerfilEditarVariavelPage(),
-            "/perfil/configuracao":(context)=> ConfiguracaoPage(),
+              //perfil
+              "/perfil": (context) => PerfilPage(),
+              "/perfil/editar_variavel": (context) => PerfilEditarVariavelPage(),
+              "/perfil/configuracao":(context)=> ConfiguracaoPage(),
 
-            "/questionario/home":(context) => QuestionarioHomePage(),
-            "/pergunta/home":(context) => PerguntaHomePage(),
-            "/aplicacao/home":(context) => AplicacaoHomePage(),
-            "/resposta/home":(context) => RespostaHomePage(),
-            "/sintese/home":(context) => SinteseHomePage(),
+              "/questionario/home":(context) => QuestionarioHomePage(),
+              "/pergunta/home":(context) => PerguntaHomePage(),
+              "/aplicacao/home":(context) => AplicacaoHomePage(),
+              "/resposta/home":(context) => RespostaHomePage(),
+              "/sintese/home":(context) => SinteseHomePage(),
 
-            //produto
-            "/produto": (context) => ProductPage(),
-            "/produto/adicionar_editar": (context) => AddEditProduct(),
-            "/produto/lista": (context) => ProductList(),
-            "/produto/visual": (context) => ProductVisual(),
-            "/produto/editar_visual": (context) => EditVisual(),
+              //produto
+              "/produto": (context) => ProductPage(),
+              "/produto/adicionar_editar": (context) => AddEditProduct(),
+              "/produto/lista": (context) => ProductList(),
+              "/produto/visual": (context) => ProductVisual(),
+              "/produto/editar_visual": (context) => EditVisual(),
 
-            //comunicacao
-            "/noticias/noticias_visualizadas": (context) =>
-                NoticiasVisualizadasPage(),
-            "/comunicacao": (context) => CommunicationPage(),
-            "/comunicacao/criar_editar": (context) => CommunicationCreateEdit(),
+              //comunicacao
+              "/noticias/noticias_visualizadas": (context) =>
+                  NoticiasVisualizadasPage(),
+              "/comunicacao": (context) => CommunicationPage(),
+              "/comunicacao/criar_editar": (context) => CommunicationCreateEdit(),
 
-            //administração
-            "/administracao/home":(context) => AdministracaoHomePage(),
-          },
+              //administração
+              "/administracao/home":(context) => AdministracaoHomePage(),
+            },
+          ),
         ),
       ),
     );
