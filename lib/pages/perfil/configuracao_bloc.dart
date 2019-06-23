05cc812e7feb7ea5c6c50e8cc2a87688647ed56f
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-06-23 02:20:34 -0300

    usa upload bloc e implementa processo parcial do formulario

--- lib/pages/perfil/configuracao_bloc.dart
+++ lib/pages/perfil/configuracao_bloc.dart
@@ -5,8 +5,26 @@ import 'package:rxdart/rxdart.dart';
 import 'package:pmsbmibile3/models/perfis_usuarios_model.dart';
 import 'package:pmsbmibile3/models/eixos_model.dart';
 import 'package:pmsbmibile3/models/setores_censitarios_model.dart';
+import 'package:pmsbmibile3/models/arquivos_usuarios_model.dart';
+import 'package:pmsbmibile3/state/upload_bloc.dart';
+
+class FormState {
+  PerfilUsuarioModel currentPerfilUsuario;
+  String nomeProjeto;
+  String numeroCelular;
+
+  String filePath;
+  dynamic imagemPerfil;
+  String imagemPerfilUrl;
+
+  @override
+  String toString() => "$nomeProjeto, $numeroCelular, $filePath";
+}
 
 class ConfiguracaoBloc {
+  final formState = FormState();
+  final uploadBloc = UploadBloc();
+
   BehaviorSubject<String> _userId = BehaviorSubject<String>();
 
   BehaviorSubject<PerfilUsuarioModel> _perfil =
@@ -21,34 +39,42 @@ class ConfiguracaoBloc {
 
   Stream<List<SetorCensitarioModel>> get setores => _setoresCensitarios.stream;
 
+  //numero celular
   BehaviorSubject<String> _numeroCelular = BehaviorSubject<String>();
 
   get numeroCelular => _numeroCelular.stream;
 
   Function get updateNumeroCelular => _numeroCelular.sink.add;
 
+  //nome projeto
   BehaviorSubject<String> _nomeProjeto = BehaviorSubject<String>();
 
   Function get updateNomeProjeto => _nomeProjeto.sink.add;
+
+  //imagem perfil
   BehaviorSubject<String> _imagemPerfil = BehaviorSubject<String>();
 
   Function get updateImagemPerfil => _imagemPerfil.sink.add;
 
+  //form
   StreamController<bool> _processForm = BehaviorSubject<bool>();
-  StreamController<bool> _validForm = BehaviorSubject<bool>();
-
-  get isValidForm => _validForm.stream;
 
   get processForm => _processForm.sink.add;
-  Observable<bool> save;
+
+  Observable<bool> formSaved;
 
   ConfiguracaoBloc() {
     //retorna somente id do usuario caso esteja logado
-    FirebaseAuth.instance.onAuthStateChanged.where((user) => user != null).map((user)=>user.uid).pipe(_userId);
+    FirebaseAuth.instance.onAuthStateChanged
+        .where((user) => user != null)
+        .map((user) => user.uid)
+        .pipe(_userId);
 
     //pega lista de eixos
-
-    Firestore.instance.collection(SetorCensitarioModel.collection).snapshots().map((snap) {
+    Firestore.instance
+        .collection(SetorCensitarioModel.collection)
+        .snapshots()
+        .map((snap) {
       return snap.documents
           .map((doc) =>
               SetorCensitarioModel.fromMap({"id": doc.documentID, ...doc.data}))
@@ -56,22 +82,26 @@ class ConfiguracaoBloc {
     }).pipe(_setoresCensitarios);
 
     _userId.stream.listen(setUpUser);
-    save = Observable.combineLatest2(
-      _numeroCelular.stream,
-      _nomeProjeto.stream,
-      (String numeroCelular, String nomeProjeto) {
-        print("dados $numeroCelular, $nomeProjeto");
-        return true;
-      },
-    );
-    save.pipe(_validForm);
+
+    //update state
+    _perfil.stream.listen(perfilUpdateFormState);
+    _numeroCelular.listen(numeroCelularUpdateState);
+    _nomeProjeto.listen(nomeProjetoUpdateState);
+    _imagemPerfil.listen(_imagemPerfilUpload);
+    uploadBloc.arquivo.listen(_imagemPerfilUpdateState); //update informação do perfil
+
+    //processa formulario
+    formSaved = Observable.combineLatest2(
+        _processForm.stream, _userId.stream, processFormState);
+    formSaved.listen((_) => _);
   }
 
   PerfilUsuarioModel perfilSnapToInstance(DocumentSnapshot snap) {
-    return PerfilUsuarioModel.fromMap({
+    var user = PerfilUsuarioModel.fromMap({
       "id": snap.documentID,
       ...snap.data,
     });
+    return user;
   }
 
   void setUpUser(String userId) {
@@ -79,13 +109,38 @@ class ConfiguracaoBloc {
         .collection(PerfilUsuarioModel.collection)
         .document(userId)
         .snapshots()
+        .where((snap) => snap.exists)
         .map(perfilSnapToInstance)
         .pipe(_perfil);
   }
 
-  void validFormData(bool v, StreamSink<bool> sink) {
-    print("validade form");
-    sink.add(true);
+  void numeroCelularUpdateState(String numeroCelular) {
+    formState.numeroCelular = numeroCelular;
+  }
+
+  void nomeProjetoUpdateState(String nomeProjeto) {
+    formState.nomeProjeto = nomeProjeto;
+  }
+
+  void perfilUpdateFormState(PerfilUsuarioModel perfil) {
+    formState.numeroCelular = perfil.celular;
+    formState.nomeProjeto = perfil.nomeProjeto;
+  }
+
+  bool processFormState(bool flag, String userId) {
+    Firestore.instance
+        .collection(PerfilUsuarioModel.collection)
+        .document(userId)
+        .setData({
+      ...PerfilUsuarioModel(
+        id: userId,
+        nomeProjeto: formState.nomeProjeto,
+        celular: formState.numeroCelular,
+        imagemPerfil: formState.imagemPerfil,
+        imagemPerfilUrl: formState.imagemPerfilUrl,
+      ).toMap(),
+    }, merge: true);
+    return true;
   }
 
   void dispose() {
@@ -93,5 +148,18 @@ class ConfiguracaoBloc {
     _processForm.close();
     _nomeProjeto.close();
     _userId.close();
+    _setoresCensitarios.close();
+    _eixos.close();
+  }
+
+  //upload file
+  void _imagemPerfilUpload(String filepath) {
+    formState.filePath = filepath;
+    uploadBloc.uploadFromPath(formState.filePath);
+  }
+
+  void _imagemPerfilUpdateState(ArquivoUsuarioModel arquivo) {
+    formState.imagemPerfilUrl = arquivo.url;
+    formState.imagemPerfil = Firestore.instance.collection(ArquivoUsuarioModel.collection).document(arquivo.id);
   }
 }
