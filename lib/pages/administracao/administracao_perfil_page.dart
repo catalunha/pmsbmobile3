44a25fd6b00dc3531d2d705293d97c4ae03daf45
catalunha <catalunha.mj@gmail.com>
2019-07-10 10:15:31 -0300

    adm perfil expanded correto

--- lib/pages/administracao/administracao_perfil_page.dart
+++ lib/pages/administracao/administracao_perfil_page.dart
@@ -42,76 +42,78 @@ class AdministracaoPerfilPage extends StatelessWidget {
             );
           }
           return Container(
-            child: Column(
-              children: <Widget>[
-                Padding(padding: EdgeInsets.all(10)),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                        flex: 3,
-                        child: SquareImage(
-                          image: NetworkImage(snapshot.data.safeImagemPerfilUrl),
-                        )),
-                    Expanded(
+              child: Column(children: <Widget>[
+            Padding(padding: EdgeInsets.all(10)),
+            Expanded(
+              flex: 2,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                      flex: 3,
+                      child: SquareImage(
+                        image: NetworkImage(snapshot.data.safeImagemPerfilUrl),
+                      )),
+                  Expanded(
                       flex: 5,
                       child: Container(
                         padding: EdgeInsets.only(left: 6),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Text("ID: ${snapshot.data.id.substring(0,10)}"),
+                            Text("ID: ${snapshot.data.id.substring(0, 10)}"),
                             Text("Nome: ${snapshot.data.nomeProjeto}"),
                             Text("Celular: ${snapshot.data.celular}"),
                             Text("Email: ${snapshot.data.email}"),
                             Text("Eixo: ${snapshot.data.eixoNome}"),
                           ],
                         ),
-                    )),
-                Padding(
-                    padding: EdgeInsets.all(5),
-                    child: Text(
-                      "Perfil do usuario:",
-                      style: TextStyle(fontSize: 16),
-                    )),
-                Padding(padding: EdgeInsets.all(10)),
-                Expanded(
-                  flex: 6,
-                  child: StreamBuilder<List<VariavelUsuarioModel>>(
-                      stream: bloc.variavelUsuarioModelStream,
-                      builder: (context, snapshot) {
-                        if (snapshot.hasError) {
-                          return Center(
-                            child: Text("Erro"),
-                          );
-                        }
-                        if (!snapshot.hasData) {
-                          return Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                        return ListView(
-                          children: <Widget>[
-                            ...snapshot.data.map((variavel) {
-                              return Card(
-                                  child: ListTile(
-                                title: Text(
-                                  "${variavel.nome}:",
-                                  style: TextStyle(fontSize: 14),
-                                ),
-                                subtitle: Text(
-                                  "${variavel.conteudo}",
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ));
-                            }).toList()
-                          ],
-                        );
-                      }),
-                ),
-              ],
+                      ))
+                ],
+              ),
             ),
-          );
+            Padding(
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  "Perfil do usuario:",
+                  style: TextStyle(fontSize: 16),
+                )),
+            Padding(padding: EdgeInsets.all(10)),
+            Expanded(
+              flex: 6,
+              child: StreamBuilder<List<VariavelUsuarioModel>>(
+                  stream: bloc.variavelUsuarioModelStream,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text("Erro"),
+                      );
+                    }
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    return ListView(
+                      children: <Widget>[
+                        ...snapshot.data.map((variavel) {
+                          return Card(
+                              child: ListTile(
+                            title: Text(
+                              "${variavel.nome}:",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                            subtitle: Text(
+                              "${variavel.conteudo}",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ));
+                        }).toList()
+                      ],
+                    );
+                  }),
+            ),
+          ]));
         });
   }
 }
