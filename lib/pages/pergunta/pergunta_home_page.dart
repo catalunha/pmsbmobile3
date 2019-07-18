f9520cebd763bba27ff0594bc15bd5ba94473c6c
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-07-18 00:57:39 -0300

    lista perguntas de um questionario selecionado

--- lib/pages/pergunta/pergunta_home_page.dart
+++ lib/pages/pergunta/pergunta_home_page.dart
@@ -1,112 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:pmsbmibile3/bootstrap.dart';
+import 'package:pmsbmibile3/components/eixo.dart';
+import 'package:pmsbmibile3/models/pergunta_model.dart';
+import 'package:pmsbmibile3/pages/pergunta/pergunta_home_page_bloc.dart';
 
-class PerguntaHomePage extends StatefulWidget {
-  @override
-  _PerguntaHomePageState createState() => _PerguntaHomePageState();
-}
+class PerguntaHomePage extends StatelessWidget {
+  String _questionarioId;
+  final bloc = PerguntaHomePageBloc(Bootstrap.instance.firestore);
 
-class _PerguntaHomePageState extends State<PerguntaHomePage> {
-  
-  List<String> _perguntas = [
-    "01 - pergunta texto",
-    "02 - pergunta imagem",
-    "03 - pergunta numero",
-    "04 - pergunta coordenada",
-    "05 - pergunta escolha unica",
-    "06 - pergunta escolha multipla",
-  ];
-
-  String _eixo = "eixo exemplo";
-  String _questionario = "questionarios exemplo";
-
-  _listaProdutos() {
-    return Builder(
-        builder: (BuildContext context) => new Container(
-              child: _perguntas.length >= 0
-                  ? new ListView.builder(
-                      itemCount: _perguntas.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                            elevation: 10,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                ListTile(
-                                  title: Text(_perguntas[index]),
-                                ),
-                                ButtonTheme.bar(
-                                  child: ButtonBar(
-                                    children: <Widget>[
-                                      IconButton(
-                                        icon: Icon(Icons.arrow_downward),
-                                        onPressed: () {
-                                          //Mover pergunta para baixo na ordem
-                                        },
-                                      ),
-                                      IconButton(
-                                        icon: Icon(Icons.arrow_upward),
-                                        onPressed: () {
-                                          //Mover pergunta para cima na ordem
-                                        },
-                                      ),
-                                      IconButton(
-                                        icon: Icon(Icons.edit),
-                                        onPressed: () {
-                                          //Editar pergunta
-                                         
-                                        },
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ));
-                      })
-                  : Container(),
-            ));
+  Widget _questionarioAtual(context) {
+    return StreamBuilder<PerguntaHomePageBlocState>(
+        stream: bloc.state,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("ERROR"),
+            );
+          }
+          if (!snapshot.hasData) {
+            return Center(
+              child: Text("SEM DADOS"),
+            );
+          }
+          final questionario = snapshot.data.questionarioInstance;
+          return Text(
+            "Questionario - ${questionario.nome}",
+            style: TextStyle(fontSize: 16, color: Colors.blue),
+          );
+        });
   }
 
-  Widget _body() {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 10),
-          child: Text(
-            "Eixo - $_eixo",
-            style: TextStyle(fontSize: 16, color: Colors.blue),
-          ),
+  List<Widget> _preambulo(context) {
+    return [
+      Padding(
+        padding: EdgeInsets.only(top: 10),
+        child: Center(
+          child: EixoAtualUsuario(),
         ),
-        Padding(
-          padding: EdgeInsets.only(bottom: 10),
-          child: Text(
-            "Setor - $_questionario",
-            style: TextStyle(fontSize: 16, color: Colors.blue),
-          ),
+      ),
+      Padding(
+        padding: EdgeInsets.only(bottom: 10),
+        child: Center(
+          child: _questionarioAtual(context),
         ),
-        Expanded(child: _listaProdutos()),
-        Padding(padding: EdgeInsets.all(30))
-      ],
+      ),
+    ];
+  }
+
+  Widget _body(context) {
+    return StreamBuilder<List<PerguntaModel>>(
+      stream: bloc.perguntas,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("ERROR"),
+          );
+        }
+        if (!snapshot.hasData) {
+          return Center(
+            child: Text("SEM DADOS"),
+          );
+        }
+        return ListView(
+          children: [
+            ..._preambulo(context),
+            ...snapshot.data.map((pergunta) => PerguntaItem(pergunta)).toList(),
+            Padding(padding: EdgeInsets.all(30)),
+          ],
+        );
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    _questionarioId = ModalRoute.of(context).settings.arguments;
+    bloc.dispatch(
+        UpdateQuestionarioIdPerguntaHomePageBlocEvent(_questionarioId));
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
         centerTitle: true,
-        title: Text("Criar pergunta do tipo"),
+        title: Text("Lista de Perguntas"),
       ),
-      body: _body(),
+      body: _body(context),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           //Adicionar um nova pergunta
-          Navigator.pushNamed(context, '/pergunta/criar_pergunta');
+          Navigator.pushNamed(context, '/pergunta/criar_pergunta', arguments: _questionarioId);
         },
         child: Icon(Icons.add),
         backgroundColor: Colors.blue,
-
       ),
     );
+  }
+}
+
+class PerguntaItem extends StatelessWidget {
+  final PerguntaModel _pergunta;
+
+  PerguntaItem(this._pergunta);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            title: Text(_pergunta.titulo),
+          ),
+          ButtonTheme.bar(
+            child: ButtonBar(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_downward),
+                  onPressed: () {
+                    //Mover pergunta para baixo na ordem
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_upward),
+                  onPressed: () {
+                    //Mover pergunta para cima na ordem
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    //Editar pergunta
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+    ;
   }
 }
---
+++ lib/pages/pergunta/pergunta_home_page_bloc.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'package:pmsbmibile3/models/questionario_model.dart';
+import 'package:pmsbmibile3/models/pergunta_model.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:firestore_wrapper/firestore_wrapper.dart' as fsw;
+
+class PerguntaHomePageBlocEvent {}
+
+class UpdateQuestionarioIdPerguntaHomePageBlocEvent
+    extends PerguntaHomePageBlocEvent {
+  final String questionarioId;
+
+  UpdateQuestionarioIdPerguntaHomePageBlocEvent(this.questionarioId);
+}
+
+class PerguntaHomePageBlocState {
+  QuestionarioModel questionarioInstance;
+}
+
+class PerguntaHomePageBloc {
+  PerguntaHomePageBloc(this._firestore) {
+    _inputController.listen(_handleInput);
+  }
+
+  final fsw.Firestore _firestore;
+
+  final _state = PerguntaHomePageBlocState();
+
+  final _inputController = BehaviorSubject<PerguntaHomePageBlocEvent>();
+
+  Function get dispatch => _inputController.add;
+
+  final _outputController = BehaviorSubject<PerguntaHomePageBlocState>();
+
+  Stream<PerguntaHomePageBlocState> get state => _outputController.stream;
+
+  final _perguntasController = BehaviorSubject<List<PerguntaModel>>();
+  StreamSubscription<List<PerguntaModel>> _perguntaSubscription;
+
+  Stream<List<PerguntaModel>> get perguntas => _perguntasController.stream;
+
+  void dispose() {
+    _inputController.close();
+    _outputController.close();
+    _perguntasController.close();
+  }
+
+  void _handleInput(PerguntaHomePageBlocEvent event) async {
+    if (event is UpdateQuestionarioIdPerguntaHomePageBlocEvent) {
+      final ref = _firestore
+          .collection(QuestionarioModel.collection)
+          .document(event.questionarioId);
+      await ref.get().then((data) {
+        _state.questionarioInstance =
+            QuestionarioModel(id: data.documentID).fromMap(data.data);
+      });
+
+      final perguntasRef = _firestore
+          .collection(PerguntaModel.collection)
+          .where("questionario.id", isEqualTo: event.questionarioId);
+      final perguntasStream = perguntasRef.snapshots().map((snp) {
+        print(snp.documents.length);
+        return snp.documents.map((doc) {
+          return PerguntaModel(id: doc.documentID).fromMap(doc.data);
+        }).toList();
+      });
+      await _perguntaSubscription?.cancel();
+      _perguntaSubscription = perguntasStream.listen((data){
+        _perguntasController.add(data);
+      });
+    }
+    _outputController.add(_state);
+  }
+}
