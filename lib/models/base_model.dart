8124e613ff6cbd75e0ec8cbc10afc235569c0f57
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-07-19 11:13:23 -0300

    remove classe não usada

--- lib/models/base_model.dart
+++ lib/models/base_model.dart
@@ -1,19 +1,3 @@
-class Model{
-  static final String collection = "";
-
-  dynamic _referenciaFirestore;
-  dynamic get ref => _referenciaFirestore;
-  set ref (dynamic ref) => _referenciaFirestore = ref;
-
-  @override
-  String toString() {
-    return toMap().toString();
-  }
-
-  Map<dynamic, dynamic> toMap(){
-    return {};
-  }
-}
 
 /// classe base para todos os modelos de dados originados no firestore
 abstract class FirestoreModel{
