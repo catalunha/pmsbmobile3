f2c9364aaddb7a698821411515c97beb9071e146
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-07-28 19:40:11 -0300

    implementação parcial de validação, salvar e editar sem a resolução de requisitos

--- lib/pages/aplicacao/momento_aplicacao_page_bloc.dart
+++ lib/pages/aplicacao/momento_aplicacao_page_bloc.dart
@@ -1,6 +1,6 @@
 import 'package:pmsbmibile3/models/pergunta_model.dart';
 import 'package:pmsbmibile3/models/questionario_model.dart';
-import 'package:rxdart/rxdart.dart';
+import 'package:pmsbmibile3/models/usuario_model.dart';
 import 'package:firestore_wrapper/firestore_wrapper.dart' as fsw;
 import 'package:pmsbmibile3/state/bloc.dart';
 
@@ -38,12 +38,18 @@ class SelecionarQuestionarioMomentoAplicacaoPageBlocEvent
   SelecionarQuestionarioMomentoAplicacaoPageBlocEvent(this.questionario);
 }
 
+class UpdateUsuarioMomentoAplicacaoPageBlocEvent
+    extends MomentoAplicacaoPageBlocEvent {
+  final UsuarioModel usuario;
+
+  UpdateUsuarioMomentoAplicacaoPageBlocEvent(this.usuario);
+}
+
 class MomentoAplicacaoPageBlocState {
   String questionarioAplicadoID;
   String referencia;
   String usuarioID;
   String usuarioNome;
-  String questionarioID;
 
   bool isBound = false;
   bool isValid = false;
@@ -61,7 +67,6 @@ class MomentoAplicacaoPageBlocState {
     this.referencia,
     this.usuarioID,
     this.usuarioNome,
-    this.questionarioID,
   });
 }
 
@@ -76,11 +81,53 @@ class MomentoAplicacaoPageBloc
     return MomentoAplicacaoPageBlocState();
   }
 
+  void validateState() {
+    bool valid = true;
+
+    if(!currentState.isBound && currentState.questionario == null){
+      valid = false;
+    }
+
+    if (currentState.referencia == null ||
+        currentState.referencia.trim().isEmpty) {
+      valid = false;
+    }
+
+    currentState.isValid = valid;
+  }
+
   @override
   Future<void> mapEventToState(MomentoAplicacaoPageBlocEvent event) async {
+
+    if (event is UpdateUsuarioMomentoAplicacaoPageBlocEvent) {
+      currentState.usuarioNome = event.usuario.nome;
+      currentState.usuarioID = event.usuario.id;
+    }
+    if (event is UpdateIDMomentoAplicacaoPageBlocEvent) {
+      if (event.questionarioAplicadoID != null) {
+        final questionarioAplicadoRef = _firestore
+            .collection(QuestionarioAplicadoModel.collection)
+            .document(event.questionarioAplicadoID);
+        final questionarioAplicadoSnap = await questionarioAplicadoRef.get();
+        if (questionarioAplicadoSnap.exists) {
+          currentState.isBound = true;
+          currentState.questionario =
+              QuestionarioAplicadoModel(id: questionarioAplicadoSnap.documentID)
+                  .fromMap(questionarioAplicadoSnap.data);
+          dispatch(CarregarListaPerguntasMomentoAplicacaoPageBlocEvent());
+
+          final QuestionarioAplicadoModel q = currentState.questionario;
+          dispatch(UpdateReferenciaMomentoAplicacaoPageBlocEvent(q.referencia));
+        } else {
+          currentState.isBound = false;
+        }
+      }
+    }
+
     if (event is UpdateReferenciaMomentoAplicacaoPageBlocEvent) {
       currentState.referencia = event.referencia;
     }
+
     if (event is CarregarListaQuestionarioMomentoAplicacaoPageBlocEvent) {
       final questionariosSnap = await _firestore
           .collection(QuestionarioModel.collection)
@@ -92,45 +139,97 @@ class MomentoAplicacaoPageBloc
 
     if (event is SelecionarQuestionarioMomentoAplicacaoPageBlocEvent) {
       currentState.questionario = event.questionario;
+      currentState.isBound = false;
       dispatch(CarregarListaPerguntasMomentoAplicacaoPageBlocEvent());
     }
 
     if (event is CarregarListaPerguntasMomentoAplicacaoPageBlocEvent) {
+      final String collection = currentState.isBound
+          ? PerguntaAplicadaModel.collection
+          : PerguntaModel.collection;
+
       final perguntasSnap = await _firestore
-          .collection(PerguntaModel.collection)
-          .where("questionario.id",
-              isEqualTo: currentState.questionario.id)
+          .collection(collection)
+          .where("questionario.id", isEqualTo: currentState.questionario.id)
           .getDocuments();
-      currentState.perguntas = perguntasSnap.documents
-          .map((doc) => PerguntaModel(id: doc.documentID).fromMap(doc.data))
-          .toList();
-      currentState.requisitos.clear();
-      currentState.requisitosSelecionados.clear();
-      currentState.perguntas.forEach((pergunta) {
-        pergunta.requisitos.forEach((id, requisito) {
-          currentState.requisitos[id] = requisito;
+
+      currentState.perguntas = perguntasSnap.documents.map((doc) {
+        if (currentState.isBound)
+          return PerguntaAplicadaModel(id: doc.documentID).fromMap(doc.data);
+        else
+          return PerguntaModel(id: doc.documentID).fromMap(doc.data);
+      }).toList();
+      if (currentState.isBound) {
+        currentState.requisitos.clear();
+        currentState.requisitosSelecionados.clear();
+
+        currentState.perguntas.forEach((pergunta) {
+          pergunta.requisitos.forEach((id, requisito) {
+            currentState.requisitos[id] = requisito;
+          });
         });
-      });
+      }
     }
 
     if (event is DeleteMomentoAplicacaoPageBlocEvent) {
       final ref = _firestore
           .collection(QuestionarioAplicadoModel.collection)
-          .document(currentState.questionarioAplicadoID);
+          .document(currentState.questionario.id);
       await ref.delete();
+      //deleta perguntas aplicadas
+      final perguntasAplicadasRef = _firestore
+          .collection(PerguntaAplicadaModel.collection)
+          .where("questionario.id", isEqualTo: currentState.questionario.id);
+      perguntasAplicadasRef.getDocuments().then((query) {
+        query.documents.forEach((doc) {
+          doc.reference.delete();
+        });
+      });
     }
-
     if (event is SaveMomentoAplicacaoPageBlocEvent) {
-      final ref = _firestore
-          .collection(QuestionarioAplicadoModel.collection)
-          .document(currentState.questionarioAplicadoID);
-      final model = QuestionarioAplicadoModel(
-        referencia: currentState.referencia,
-        aplicador: UsuarioQuestionario(
-            id: currentState.usuarioID, nome: currentState.usuarioNome),
-        aplicado: DateTime.now(),
-      );
-      ref.setData(model.toMap(), merge: true);
+      if (!currentState.isBound) {
+        final ref = _firestore
+            .collection(QuestionarioAplicadoModel.collection)
+            .document();
+        criar_questionario_aplicado(ref);
+      } else {
+        final ref = _firestore
+            .collection(QuestionarioAplicadoModel.collection)
+            .document(currentState.questionario.id);
+        editar_questionario_aplicado(ref);
+      }
     }
+    validateState();
+  }
+
+  void criar_questionario_aplicado(fsw.DocumentReference ref) {
+    //criar questionario aplicado
+    QuestionarioAplicadoModel model =
+        QuestionarioAplicadoModel().fromMap(currentState.questionario.toMap());
+    final usuario = UsuarioQuestionario(
+        id: currentState.usuarioID, nome: currentState.usuarioNome);
+
+    model.referencia = currentState.referencia;
+    model.aplicador = usuario;
+    model.aplicado = DateTime.now();
+
+    ref.setData(model.toMap(), merge: true);
+
+    //cria pergutnas aplicadas
+    final perguntasAplicadasRef =
+        _firestore.collection(PerguntaAplicadaModel.collection);
+    currentState.perguntas.forEach((pergunta) {
+      final model =
+          PerguntaAplicadaModel(id: pergunta.id).fromMap(pergunta.toMap());
+      model.questionario.id = ref.documentID;
+      final perguntaAplicadaRef = perguntasAplicadasRef.document();
+      perguntaAplicadaRef.setData(model.toMap());
+    });
+  }
+
+  void editar_questionario_aplicado(fsw.DocumentReference ref) {
+    final model =
+        QuestionarioAplicadoModel(referencia: currentState.referencia);
+    ref.setData(model.toMap(), merge: true);
   }
 }
