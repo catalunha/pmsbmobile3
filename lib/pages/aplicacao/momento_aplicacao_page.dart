9391b4c5f641180e5d9ded631027c392721e5a33
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-07-28 19:04:29 -0300

    separa requisitos e referencia em outros dois widgets mostra lista de requisitos para serem editadas as referencias somente se for na edição do questionario

--- lib/pages/aplicacao/momento_aplicacao_page.dart
+++ lib/pages/aplicacao/momento_aplicacao_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:pmsbmibile3/bootstrap.dart';
 import 'package:pmsbmibile3/pages/aplicacao/momento_aplicacao_page_bloc.dart';
+import 'package:pmsbmibile3/state/auth_bloc.dart';
 
 class MomentoAplicacaoPage extends StatefulWidget {
   final String usuarioID;
   final String questionarioAplicadoID;
+  final AuthBloc authBloc;
 
-  const MomentoAplicacaoPage(
+  const MomentoAplicacaoPage(this.authBloc,
       {Key key, this.usuarioID, this.questionarioAplicadoID})
       : super(key: key);
 
@@ -27,6 +29,10 @@ class _MomentoAplicacaoPageState extends State<MomentoAplicacaoPage> {
   @override
   void initState() {
     super.initState();
+    widget.authBloc.perfil.listen((usuario) {
+      bloc.dispatch(UpdateUsuarioMomentoAplicacaoPageBlocEvent(usuario));
+    });
+
     bloc.dispatch(
         UpdateIDMomentoAplicacaoPageBlocEvent(widget.questionarioAplicadoID));
   }
@@ -41,6 +47,7 @@ class _MomentoAplicacaoPageState extends State<MomentoAplicacaoPage> {
                 color: Colors.red,
                 onPressed: () {
                   bloc.dispatch(DeleteMomentoAplicacaoPageBlocEvent());
+                  Navigator.pop(context);
                 },
                 child: Row(
                   children: <Widget>[
@@ -50,21 +57,6 @@ class _MomentoAplicacaoPageState extends State<MomentoAplicacaoPage> {
                 ))),
       ],
     ));
-  }
-
-  _referenciaTexto() {
-    return Padding(
-        padding: EdgeInsets.all(5.0),
-        child: TextField(
-          onChanged: (text) {
-            bloc.dispatch(UpdateReferenciaMomentoAplicacaoPageBlocEvent(text));
-          },
-          keyboardType: TextInputType.multiline,
-          maxLines: null,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-          ),
-        ));
   }
 
   _listaDadosSuperior() {
@@ -138,59 +130,9 @@ class _MomentoAplicacaoPageState extends State<MomentoAplicacaoPage> {
             padding: EdgeInsets.all(5),
             child: Text("Referencia: Local/Pessoa/Momento na aplicação:",
                 style: TextStyle(color: Colors.blue, fontSize: 15))),
-        _referenciaTexto(),
+        ReferenciaInput(bloc),
         Divider(color: Colors.black87),
-        Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-          ListTile(
-            trailing: IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {
-                  //selecionar requisitos
-                  Navigator.pushNamed(context, '/aplicacao/definir_requisitos');
-                }),
-            title: Text("Lista de requisitos:"),
-          ),
-          StreamBuilder<MomentoAplicacaoPageBlocState>(
-            stream: bloc.state,
-            builder: (context, snapshot) {
-              //if(!snapshot.hasData) return Container();
-              final requisitos = snapshot?.data?.requisitos;
-              final requisitosMap = requisitos != null ? requisitos : {};
-              return Column(
-                children: requisitosMap
-                    .map((k, r) {
-                      return MapEntry(
-                          k,
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              FlatButton(
-                                child: Text(
-                                  "${r.referencia}",
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                                onPressed: (){},
-                              ),
-                              snapshot.data.requisitosSelecionados
-                                      .containsKey(k)
-                                  ? Icon(
-                                      Icons.check_circle,
-                                      color: Colors.blue,
-                                    )
-                                  : Icon(
-                                      Icons.check_circle_outline,
-                                      color: Colors.red,
-                                    ),
-                            ],
-                          ));
-                    })
-                    .values
-                    .toList(),
-              );
-            },
-          ),
-        ]),
-        Divider(color: Colors.black87),
+        ListaRequisitos(bloc),
         _botaoDeletar()
       ],
     );
@@ -223,6 +165,118 @@ class _MomentoAplicacaoPageState extends State<MomentoAplicacaoPage> {
                   snapshot.data.isValid ? Colors.blue : Colors.grey,
             );
           }),
+    );
+  }
+}
+
+class ListaRequisitos extends StatelessWidget {
+  final MomentoAplicacaoPageBloc bloc;
+
+  const ListaRequisitos(this.bloc, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<MomentoAplicacaoPageBlocState>(
+      stream: bloc.state,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData || !snapshot.data.isBound)
+          return Center(
+            child: Text("Escolher requisitos depois de salvar"),
+          );
+        final requisitos = snapshot?.data?.requisitos;
+        final requisitosMap = requisitos != null ? requisitos : {};
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              trailing: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    //selecionar requisitos
+                    Navigator.pushNamed(
+                        context, '/aplicacao/definir_requisitos');
+                  }),
+              title: Text("Lista de requisitos:"),
+            ),
+            ...requisitosMap
+                .map((k, r) {
+                  return MapEntry(
+                      k,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          FlatButton(
+                            child: Text(
+                              "${r.referencia}",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            onPressed: () {},
+                          ),
+                          snapshot.data.requisitosSelecionados.containsKey(k)
+                              ? Icon(
+                                  Icons.check_circle,
+                                  color: Colors.blue,
+                                )
+                              : Icon(
+                                  Icons.check_circle_outline,
+                                  color: Colors.red,
+                                ),
+                        ],
+                      ));
+                })
+                .values
+                .toList(),
+            Divider(color: Colors.black87),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class ReferenciaInput extends StatefulWidget {
+  final MomentoAplicacaoPageBloc bloc;
+
+  const ReferenciaInput(this.bloc, {Key key}) : super(key: key);
+
+  @override
+  _ReferenciaInput createState() {
+    return _ReferenciaInput();
+  }
+}
+
+class _ReferenciaInput extends State<ReferenciaInput> {
+  final _controller = TextEditingController();
+  bool initial = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<MomentoAplicacaoPageBlocState>(
+      stream: widget.bloc.state,
+      builder: (context, snapshot) {
+        if (initial &&
+            _controller.text.isEmpty &&
+            snapshot.hasData &&
+            snapshot.data.referencia != null) {
+          initial = false;
+          _controller.text = snapshot.data.referencia;
+        }
+        return Padding(
+            padding: EdgeInsets.all(5.0),
+            child: TextField(
+              controller: _controller,
+              onChanged: (text) {
+                widget.bloc.dispatch(
+                    UpdateReferenciaMomentoAplicacaoPageBlocEvent(text));
+              },
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ));
+      },
     );
   }
 }
