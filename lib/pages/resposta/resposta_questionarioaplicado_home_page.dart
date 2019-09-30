32fce4f2f005226e208a2604435b8cf5e344a969
Prof Catalunha <catalunha.mj@gmail.com>
2019-09-30 18:04:53 -0300

    #288 relatorio pdf make atualizado.

--- lib/pages/resposta/resposta_questionarioaplicado_home_page.dart
+++ lib/pages/resposta/resposta_questionarioaplicado_home_page.dart
@@ -8,6 +8,7 @@ import 'package:pmsbmibile3/services/gerador_pdf_service.dart';
 import 'package:pmsbmibile3/services/pdf_create_service.dart';
 import 'package:pmsbmibile3/services/pdf_save_service.dart';
 import 'package:pmsbmibile3/state/auth_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class RespostaQuestionarioAplicadoHomePage extends StatefulWidget {
   final AuthBloc authBloc;
@@ -32,10 +33,6 @@ class _RespostaQuestionarioAplicadoHomePageState
   @override
   void initState() {
     super.initState();
-    // authBloc.perfil.listen((usuario) {
-    //   bloc.dispatch(UpdateUserIDAplicacaoHomePageBlocEvent(
-    //       usuario.id, usuario.eixoIDAtual.id));
-    // });
   }
 
   @override
@@ -46,49 +43,75 @@ class _RespostaQuestionarioAplicadoHomePageState
 
   _listaQuestionarioAplicado() {
     return StreamBuilder<RespostaQuestionarioAplicadoHomeState>(
-      stream: bloc.stateStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text("ERROR");
-        }
-        if (!snapshot.hasData) {
-          return Text("SEM DADOS");
-        }
-        // print('>>> snapshot.data <<< ${snapshot.data.usuarioModel.nome}');
-        // print('>>> snapshot.data <<< ${snapshot.data.usuarioModel.eixoIDAtual.id}');
-        // print('>>> snapshot.data <<< ${snapshot.data.usuarioModel.setorCensitarioID.id}');
-        final questionarios = snapshot.data.questionariosAplicados != null
-            ? snapshot.data.questionariosAplicados
-            : [];
-        return ListView(
-          children: questionarios
-              .map((q) => QuestionarioAplicadoItem(bloc, q))
-              .toList(),
-        );
-      },
-    );
+        stream: bloc.stateStream,
+        builder: (BuildContext context,
+            AsyncSnapshot<RespostaQuestionarioAplicadoHomeState> snapshot) {
+          if (snapshot.hasError) {
+            return Text("ERROR");
+          }
+          if (!snapshot.hasData) {
+            return Text("SEM DADOS");
+          }
+          if (snapshot.hasData) {
+            List<Widget> listaWdg = List<Widget>();
+            if (snapshot.data.isDataValid) {
+              for (var questionarioAplicado
+                  in snapshot.data.questionarioAplicadoList) {
+                listaWdg.add(ListTile(
+                  title: Text(
+                      'Questionário: ${questionarioAplicado.nome}\nReferência: ${questionarioAplicado.referencia}'),
+                  subtitle: Text(
+                      'Aplicador: ${questionarioAplicado.aplicador.nome}\nAplicado: ${questionarioAplicado.aplicado.toDate()}\nid: ${questionarioAplicado.id}'),
+                  trailing: snapshot.data?.relatorioPdfMakeModel?.pdfGerar !=
+                              null &&
+                          snapshot.data?.relatorioPdfMakeModel?.pdfGerar ==
+                              false &&
+                          snapshot.data?.relatorioPdfMakeModel?.pdfGerado ==
+                              true &&
+                          snapshot.data?.relatorioPdfMakeModel?.tipo ==
+                              'resposta01' &&
+                          snapshot.data?.relatorioPdfMakeModel?.document ==
+                              questionarioAplicado.id
+                      ? IconButton(
+                          tooltip: 'Ver relatório geral das tarefas recebidas.',
+                          icon: Icon(Icons.link),
+                          onPressed: () async {
+                            bloc.eventSink(GerarRelatorioPdfMakeEvent(
+                                pdfGerar: false,
+                                pdfGerado: false,
+                                tipo: 'resposta01',
+                                collection: 'QuestionarioAplicado',
+                                document: questionarioAplicado.id));
+                            launch(snapshot.data?.relatorioPdfMakeModel?.url);
+                          },
+                        )
+                      : IconButton(
+                          tooltip: 'Atualizar PDF geral das tarefas recebidas.',
+                          icon: Icon(Icons.picture_as_pdf),
+                          onPressed: () async {
+                            bloc.eventSink(GerarRelatorioPdfMakeEvent(
+                                pdfGerar: true,
+                                pdfGerado: false,
+                                tipo: 'resposta01',
+                                collection: 'QuestionarioAplicado',
+                                document: questionarioAplicado.id));
+                          },
+                        ),
+                ));
+              }
+              return ListView(
+                children: listaWdg,
+              );
+            } else {
+              return Text('Existem dados inválidos...');
+            }
+          }
+        });
   }
 
   _body() {
     return Column(
-      children: <Widget>[
-        //TODO: preambulo
-//        Padding(
-//          padding: EdgeInsets.only(top: 10),
-//          child: Text(
-//            "Eixo : $_eixo",
-//            style: TextStyle(fontSize: 16, color: Colors.blue),
-//          ),
-//        ),
-//        Padding(
-//          padding: EdgeInsets.only(top: 10),
-//          child: Text(
-//            "Setor censitário: $_setor",
-//            style: TextStyle(fontSize: 16, color: Colors.blue),
-//          ),
-//        ),
-        Expanded(child: _listaQuestionarioAplicado())
-      ],
+      children: <Widget>[Expanded(child: _listaQuestionarioAplicado())],
     );
   }
 
@@ -121,66 +144,99 @@ class _CardText extends StatelessWidget {
   }
 }
 
-class QuestionarioAplicadoItem extends StatelessWidget {
-  final QuestionarioAplicadoModel _questionario;
-  final RespostaQuestionarioAplicadoHomeBloc bloc;
-  const QuestionarioAplicadoItem(this.bloc, this._questionario, {Key key})
-      : super(key: key);
+// class QuestionarioAplicadoItem extends StatelessWidget {
+//   final QuestionarioAplicadoModel _questionario;
+//   final RespostaQuestionarioAplicadoHomeBloc bloc;
+//   const QuestionarioAplicadoItem(this.bloc, this._questionario, {Key key})
+//       : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-        elevation: 10,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // _CardText("id: ${_questionario.id}"),
-            _CardText("Questionário: ${_questionario.nome}"),
-            _CardText("Referência: ${_questionario.referencia}"),
-            _CardText("Aplicador: ${_questionario.aplicador.nome}"),
-            _CardText("Aplicado: ${_questionario.aplicado.toDate()}"),
-            _CardText("id: ${_questionario.id}"),
-            ButtonTheme.bar(
-              child: ButtonBar(
-                children: <Widget>[
-                  // IconButton(
-                  //   tooltip: 'Lista das respostas em PDF',
-                  //   icon: Icon(Icons.picture_as_pdf),
-                  //   onPressed: () async {
-                  //     var mdtext = await GeradorMdService
-                  //         .generateMdFromQuestionarioAplicadoModel(
-                  //             _questionario);
-                  //     GeradorPdfService.generatePdfFromMd(mdtext);
-                  //   },
-                  // ),
-                  IconButton(
-                    tooltip: 'Relatorio em tela',
-                    icon: Icon(Icons.text_fields),
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/resposta/pergunta",
-                          arguments: _questionario.id);
-                    },
-                  ),
-                  // IconButton(
-                  //   tooltip: 'Google Docs das respostas',
-                  //   icon: Icon(Icons.book),
-                  //   onPressed: () {
-                  //     bloc.eventSink(CreateRelatorioEvent(_questionario));
-                  //   },
-                  // ),
-                  IconButton(
-                    tooltip: 'Relatorio em PDF.',
-                    icon: Icon(Icons.picture_as_pdf),
-                    onPressed: () async {
-                      var pdf = await PdfCreateService
-                          .createPdfForQuestionarioAplicadoModel(_questionario);
-                      PdfSaveService.generatePdfAndOpen(pdf);
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
-        ));
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//         elevation: 10,
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget>[
+//             // _CardText("id: ${_questionario.id}"),
+//             _CardText("Questionário: ${_questionario.nome}"),
+//             _CardText("Referência: ${_questionario.referencia}"),
+//             _CardText("Aplicador: ${_questionario.aplicador.nome}"),
+//             _CardText("Aplicado: ${_questionario.aplicado.toDate()}"),
+//             _CardText("id: ${_questionario.id}"),
+//             ButtonTheme.bar(
+//               child: ButtonBar(
+//                 children: <Widget>[
+//                   // IconButton(
+//                   //   tooltip: 'Lista das respostas em PDF',
+//                   //   icon: Icon(Icons.picture_as_pdf),
+//                   //   onPressed: () async {
+//                   //     var mdtext = await GeradorMdService
+//                   //         .generateMdFromQuestionarioAplicadoModel(
+//                   //             _questionario);
+//                   //     GeradorPdfService.generatePdfFromMd(mdtext);
+//                   //   },
+//                   // ),
+//                   // IconButton(
+//                   //   tooltip: 'Relatorio em tela',
+//                   //   icon: Icon(Icons.text_fields),
+//                   //   onPressed: () {
+//                   //     Navigator.pushNamed(context, "/resposta/pergunta",
+//                   //         arguments: _questionario.id);
+//                   //   },
+//                   // ),
+//                   // IconButton(
+//                   //   tooltip: 'Google Docs das respostas',
+//                   //   icon: Icon(Icons.book),
+//                   //   onPressed: () {
+//                   //     bloc.eventSink(CreateRelatorioEvent(_questionario));
+//                   //   },
+//                   // ),
+//                   // IconButton(
+//                   //   tooltip: 'Relatorio em PDF.',
+//                   //   icon: Icon(Icons.picture_as_pdf),
+//                   //   onPressed: () async {
+//                   //     var pdf = await PdfCreateService
+//                   //         .createPdfForQuestionarioAplicadoModel(_questionario);
+//                   //     PdfSaveService.generatePdfAndOpen(pdf);
+//                   //   },
+//                   // ),
+
+//                   bloc.snapshot.data?.relatorioPdfMakeModel?.pdfGerar != null &&
+//                           snapshot.data?.relatorioPdfMakeModel?.pdfGerar ==
+//                               false &&
+//                           snapshot.data?.relatorioPdfMakeModel?.pdfGerado ==
+//                               true &&
+//                           snapshot.data?.relatorioPdfMakeModel?.tipo ==
+//                               'controle01'
+//                       ? IconButton(
+//                           tooltip: 'Ver relatório geral das tarefas recebidas.',
+//                           icon: Icon(Icons.link),
+//                           onPressed: () async {
+//                             bloc.eventSink(GerarRelatorioPdfMakeEvent(
+//                                 pdfGerar: false,
+//                                 pdfGerado: false,
+//                                 tipo: 'controle01',
+//                                 collection: 'Usuario',
+//                                 document: snapshot.data.usuarioID.id));
+//                             launch(snapshot.data?.relatorioPdfMakeModel?.url);
+//                           },
+//                         )
+//                       : IconButton(
+//                           tooltip: 'Atualizar PDF geral das tarefas recebidas.',
+//                           icon: Icon(Icons.picture_as_pdf),
+//                           onPressed: () async {
+//                             bloc.eventSink(GerarRelatorioPdfMakeEvent(
+//                                 pdfGerar: true,
+//                                 pdfGerado: false,
+//                                 tipo: 'controle01',
+//                                 collection: 'Usuario',
+//                                 document: snapshot.data.usuarioID.id));
+//                           },
+//                         ),
+//                 ],
+//               ),
+//             )
+//           ],
+//         ));
+//   }
+// }
