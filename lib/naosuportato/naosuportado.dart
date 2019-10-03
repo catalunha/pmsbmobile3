477b7f78a26a811ccef45b36510f67336a8f26b9
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-10-03 14:30:50 -0300

    corrige erro no endereço de biblioteca

--- lib/naosuportato/naosuportado.dart
+++ lib/naosuportato/naosuportado.dart
@@ -2,7 +2,7 @@ export 'package:pmsbmibile3/naosuportato/empty.dart';
 export 'package:pmsbmibile3/naosuportato/location.dart'
     if (dart.library.io) 'package:location/location.dart';
 export 'package:pmsbmibile3/naosuportato/url_launcher.dart'
-    if (dart.library.io) 'package:url_launcher/url_launcher';
+    if (dart.library.io) 'package:url_launcher/url_launcher.dart';
 export 'package:pmsbmibile3/naosuportato/file_picker.dart'
     if (dart.library.io) 'package:file_picker/file_picker.dart';
 export 'package:pmsbmibile3/naosuportato/firebase_messaging.dart'
