e88f97a018a2fda3a9a1a22c7016771a8719dfa7
catalunha <catalunha.mj@gmail.com>
2019-10-19 08:29:25 -0300

    painel com tabs

--- lib/pages/painel/painel_list_page.dart
+++ lib/pages/painel/painel_list_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pmsbmibile3/bootstrap.dart';
-import 'package:pmsbmibile3/components/default_scaffold.dart';
 import 'package:pmsbmibile3/pages/painel/painel_list_bloc.dart';
-import 'package:pmsbmibile3/state/auth_bloc.dart';
 import 'package:pmsbmibile3/naosuportato/url_launcher.dart'
     if (dart.library.io) 'package:url_launcher/url_launcher.dart';
 
@@ -12,7 +10,19 @@ class PainelListPage extends StatefulWidget {
 
 class _PainelListPageState extends State<PainelListPage> {
   PainelListBloc bloc;
-
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: "A"),
+    Tab(text: "B"),
+    Tab(text: "C"),
+    Tab(text: "D"),
+    Tab(text: "E"),
+    Tab(text: "F"),
+    Tab(text: "G"),
+    Tab(text: "H"),
+    Tab(text: "I"),
+    Tab(text: "J"),
+    Tab(text: "K"),
+  ];
   @override
   void initState() {
     super.initState();
@@ -27,70 +37,82 @@ class _PainelListPageState extends State<PainelListPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Adicionar/Editar itens do painel'),
-      ),
-      body: StreamBuilder<PainelListBlocState>(
-          stream: bloc.stateStream,
-          builder: (BuildContext context, AsyncSnapshot<PainelListBlocState> snapshot) {
-            if (snapshot.hasError) {
-              return Text("ERROR");
-            }
-            if (!snapshot.hasData) {
-              return Text("SEM DADOS");
-            }
-            if (snapshot.hasData) {
-              if (snapshot.data.isDataValid) {
-                List<Widget> listaWidget = List<Widget>();
-                for (var painel in snapshot.data.painelList) {
-                  Widget icone;
-                  if (painel.tipo == 'texto') {
-                    icone = Icon(Icons.text_fields);
-                  } else if (painel.tipo == 'numero') {
-                    icone = Icon(Icons.looks_one);
-                  } else if (painel.tipo == 'booleano') {
-                    icone = Icon(Icons.thumbs_up_down);
-                  } else if (painel.tipo == 'urlimagem') {
-                    icone = Icon(Icons.image);
-                  } else if (painel.tipo == 'urlarquivo') {
-                    icone = Icon(Icons.attach_file);
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Adicionar/Editar itens do painel'),
+          bottom: TabBar(
+            tabs: myTabs,
+          ),
+        ),
+        body: TabBarView(
+          children: myTabs.map((Tab tab) {
+            final String label = tab.text.toLowerCase();
+            return StreamBuilder<PainelListBlocState>(
+                stream: bloc.stateStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<PainelListBlocState> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text("ERROR");
                   }
+                  if (!snapshot.hasData) {
+                    return Text("SEM DADOS");
+                  }
+                  if (snapshot.hasData) {
+                    if (snapshot.data.isDataValid) {
+                      List<Widget> listaWidget = List<Widget>();
+                      for (var painel in snapshot.data.painelList) {
+                        Widget icone;
+                        if (painel.tipo == 'texto') {
+                          icone = Icon(Icons.text_fields);
+                        } else if (painel.tipo == 'numero') {
+                          icone = Icon(Icons.looks_one);
+                        } else if (painel.tipo == 'booleano') {
+                          icone = Icon(Icons.thumbs_up_down);
+                        } else if (painel.tipo == 'urlimagem') {
+                          icone = Icon(Icons.image);
+                        } else if (painel.tipo == 'urlarquivo') {
+                          icone = Icon(Icons.attach_file);
+                        }
 
-                  listaWidget.add(Column(children: <Widget>[
-                    Card(
-                        child: ListTile(
-                      trailing: icone,
-                      title: Text('${painel?.nome}'),
-                      subtitle:
-                          Text('Destinatário: ${painel?.usuarioQVaiResponder?.nome}\nProduto: ${painel?.produto?.nome}\nEixo: ${painel?.eixo?.nome}\nEditado por: ${painel?.usuarioQEditou?.nome}\nEm: ${painel?.modificado}\nid:${painel.id}'),
-                      onTap: () {
-                        Navigator.pushNamed(
-                          context,
-                          "/painel/crud",
-                          arguments: painel.id,
-                        );
-                      },
-                    ))
-                  ]));
-                }
-                listaWidget.add(Container(
-                  padding: EdgeInsets.only(top: 70),
-                ));
-                return ListView(
-                  children: listaWidget,
-                );
-              } else {
-                return Text('Dados inválidos...');
-              }
-            }
-          }),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.pushNamed(context, '/painel/crud', arguments: null);
-        },
-        // backgroundColor: Colors.blue,
+                        listaWidget.add(Column(children: <Widget>[
+                          Card(
+                              child: ListTile(
+                            trailing: icone,
+                            title: Text('${painel?.nome}'),
+                            subtitle: Text(
+                                'Destinatário: ${painel?.usuarioQVaiResponder?.nome}\nProduto: ${painel?.produto?.nome}\nEixo: ${painel?.eixo?.nome}\nEditado por: ${painel?.usuarioQEditou?.nome}\nEm: ${painel?.modificado}\nid:${painel.id}'),
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                "/painel/crud",
+                                arguments: painel.id,
+                              );
+                            },
+                          ))
+                        ]));
+                      }
+                      listaWidget.add(Container(
+                        padding: EdgeInsets.only(top: 70),
+                      ));
+                      return ListView(
+                        children: listaWidget,
+                      );
+                    } else {
+                      return Text('Dados inválidos...');
+                    }
+                  }
+                });
+          }).toList(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            Navigator.pushNamed(context, '/painel/crud', arguments: null);
+          },
+          // backgroundColor: Colors.blue,
+        ),
       ),
     );
   }
