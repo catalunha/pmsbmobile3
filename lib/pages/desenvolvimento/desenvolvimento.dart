8204d8424951fd614c27afe37e834aef2be4dd10
Prof Catalunha <catalunha.mj@gmail.com>
2019-10-22 12:00:51 -0300

    atualizando colUsuario

--- lib/pages/desenvolvimento/desenvolvimento.dart
+++ lib/pages/desenvolvimento/desenvolvimento.dart
@@ -138,7 +138,7 @@ class _DesenvolvimentoState extends State<Desenvolvimento> {
                     trailing: IconButton(
                       icon: Icon(Icons.menu),
                       onPressed: () async {
-                        // await atualizarRoutes('nsD07Jb8cqRy9liyX82JwDSq8d22');
+                        await atualizarRoutes('YaTtTki7PZPPHznqpVtZrW6mIa42');
                       },
                     ),
                   ),
@@ -174,22 +174,34 @@ class _DesenvolvimentoState extends State<Desenvolvimento> {
   }
 
   Future atualizarRoutes(String userId) async {
-    List<dynamic> routes = [
-      '/',
-      '/desenvolvimento',
-      '/upload',
-      '/questionario/home',
-      '/aplicacao/home',
-      '/resposta/home',
-      '/sintese/home',
-      '/produto/home',
-      '/comunicacao/home_page',
-      '/administracao/home',
-      '/controle/home'
-    ];
     final docRef = _firestore.collection(UsuarioModel.collection).document(userId);
+    var snap = await docRef.get();
+    List<dynamic> routes = List<dynamic>();
+    routes.addAll(snap.data['routes']);
+    print(routes.runtimeType);
+    routes.addAll([
+      //Drawer
+      // '/',
+      // '/upload',
+      // '/questionario/home',
+      // '/aplicacao/home',
+      // '/resposta/home',
+      // '/sintese/home',
+      // '/produto/home',
+      // '/comunicacao/home',
+      // '/administracao/home',
+      // '/controle/home',
+      // "/perfil/configuracao",
+      // endDrawer
+      '/perfil/configuracao',
+      '/perfil',
+      // '/painel/home',
+      '/modooffline',
+      "/versao",
+    ]);
 
     await docRef.setData({"routes": routes}, merge: true);
+    // await docRef.updateData({"routes": routes});
     // print('>>> ok <<< ');
   }
 
