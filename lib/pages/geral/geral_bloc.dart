795a50a0ddcde4d2e8b24f9b02e08766274e31fe
Prof Catalunha <catalunha.mj@gmail.com>
2019-10-22 17:45:55 -0300

    retirei noticias do login

---
+++ lib/pages/geral/geral_bloc.dart
@@ -0,0 +1,72 @@
+import 'package:pmsbmibile3/models/usuario_model.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:firestore_wrapper/firestore_wrapper.dart' as fw;
+
+class GeralBlocEvent {}
+
+class GetUsuarioIDEvent extends GeralBlocEvent {
+  final UsuarioModel usuarioID;
+
+  GetUsuarioIDEvent(this.usuarioID);
+}
+
+class GeralBlocState {
+  bool isDataValid = false;
+
+  UsuarioModel usuarioID;
+}
+
+class GeralBloc {
+  //Firestore
+  //Firestore
+  // final fsw.Firestore _firestore;
+  // final fw.Firestore _firestore;
+
+  final _authBloc;
+
+  //Eventos
+  final _eventController = BehaviorSubject<GeralBlocEvent>();
+  Stream<GeralBlocEvent> get eventStream => _eventController.stream;
+  Function get eventSink => _eventController.sink.add;
+
+  //Estados
+  final GeralBlocState _state = GeralBlocState();
+  final _stateController = BehaviorSubject<GeralBlocState>();
+  Stream<GeralBlocState> get stateStream => _stateController.stream;
+  Function get stateSink => _stateController.sink.add;
+
+  //Bloc
+  GeralBloc(
+    this._authBloc
+  ) {
+    eventStream.listen(_mapEventToState);
+    _authBloc.perfil.listen((usuarioID) {
+      eventSink(GetUsuarioIDEvent(usuarioID));
+    });
+  }
+
+  void dispose() async {
+    await _stateController.drain();
+    _stateController.close();
+    await _eventController.drain();
+    _eventController.close();
+  }
+
+  _validateData() {
+    _state.isDataValid = false;
+    if (_state?.usuarioID != null) {
+      _state.isDataValid = true;
+    }
+  }
+
+  _mapEventToState(GeralBlocEvent event) async {
+    if (event is GetUsuarioIDEvent) {
+      //Atualiza estado com usuario logado
+      _state.usuarioID = event.usuarioID;
+    }
+
+    _validateData();
+    if (!_stateController.isClosed) _stateController.add(_state);
+    print('event.runtimeType em GeralBloc  = ${event.runtimeType}');
+  }
+}
--- lib/pages/geral/home.dart
+++ lib/pages/geral/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pmsbmibile3/components/login_required.dart';
 import 'package:pmsbmibile3/pages/comunicacao/noticia_leitura_page.dart';
+import 'package:pmsbmibile3/pages/geral/bemvindo.dart';
 import 'package:pmsbmibile3/state/auth_bloc.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,22 +12,15 @@ final AuthBloc authBloc;
 }
 
 class _HomePageState extends State<HomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//        child: child,
-//     );
-//   }
-// }
 
-// class HomePage extends StatelessWidget {
   final AuthBloc authBloc;
   _HomePageState(this.authBloc);
 
   @override
   Widget build(BuildContext context) {
     return DefaultLoginRequired(
-      child: NoticiaLeituraPage(),
+      child: BemVindo(widget.authBloc),
+      // child: NoticiaLeituraPage(),
       authBloc: this.authBloc,
       // child: NoticiasNaoVisualizadasPage(),
     );
