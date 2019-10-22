fa49019f81e048f7ce5575155e8572de149658c6
Prof Catalunha <catalunha.mj@gmail.com>
2019-10-22 20:28:47 -0300

    versao web atualizada

--- lib/pages/geral/versao.dart
+++ lib/pages/geral/versao.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:pmsbmibile3/services/recursos.dart';
 class Versao extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,7 +9,7 @@ class Versao extends StatelessWidget {
         title: Text('Versão & Sobre'),
       ),
       body: Center(
-        child: Text("Versão 3.1.0"),
+        child: Recursos.instance.plataforma == 'android' ? Text("Versão 3.1.0"):Text("Build: 201910222020"),
       ),
     );
   }
