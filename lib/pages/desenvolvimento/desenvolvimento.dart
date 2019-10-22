deba14b8db84a50546cae3edd7fa049b23ae5519
Prof Catalunha <catalunha.mj@gmail.com>
2019-10-22 14:51:59 -0300

    endDrawer incluido no routes. col de usuarios atualizada.

--- lib/pages/desenvolvimento/desenvolvimento.dart
+++ lib/pages/desenvolvimento/desenvolvimento.dart
@@ -138,7 +138,8 @@ class _DesenvolvimentoState extends State<Desenvolvimento> {
                     trailing: IconButton(
                       icon: Icon(Icons.menu),
                       onPressed: () async {
-                        await atualizarRoutes('YaTtTki7PZPPHznqpVtZrW6mIa42');
+                        // await atualizarRoutes('YaTtTki7PZPPHznqpVtZrW6mIa42');
+                        // await atualizarRoutesTodos();
                       },
                     ),
                   ),
@@ -171,6 +172,43 @@ class _DesenvolvimentoState extends State<Desenvolvimento> {
                 ],
               );
             }));
+  }
+
+  Future atualizarRoutesTodos() async {
+    var collRef = await _firestore.collection(UsuarioModel.collection).getDocuments();
+
+    for (var documentSnapshot in collRef.documents) {
+      if (documentSnapshot.data.containsKey('routes')) {
+        List<dynamic> routes = List<dynamic>();
+
+        routes.addAll(documentSnapshot.data['routes']);
+        print(routes.runtimeType);
+        routes.addAll([
+          // Drawer
+          // '/',
+          // '/upload',
+          // '/questionario/home',
+          // '/aplicacao/home',
+          // '/resposta/home',
+          // '/sintese/home',
+          // '/produto/home',
+          // '/comunicacao/home',
+          // '/administracao/home',
+          // '/controle/home',
+          // "/perfil/configuracao",
+          // endDrawer
+          '/perfil/configuracao',
+          '/perfil',
+          // '/painel/home',
+          '/modooffline',
+          "/versao",
+        ]);
+
+        await documentSnapshot.reference.setData({"routes": routes}, merge: true);
+      } else {
+        print('Sem routes ${documentSnapshot.documentID}');
+      }
+    }
   }
 
   Future atualizarRoutes(String userId) async {
