a53cecefba08326592bf696715e870952d8be906
NataBandeira <nata.wsd@gmail.com>
2020-04-17 10:34:09 -0300

    #411 - Migracao web pagina administracao

--- lib/pages/administracao/administracao_home_page.dart
+++ lib/pages/administracao/administracao_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pmsbmibile3/components/default_scaffold.dart';
 import 'package:pmsbmibile3/models/usuario_model.dart';
@@ -6,6 +7,7 @@ import 'package:pmsbmibile3/naosuportato/url_launcher.dart'
     if (dart.library.io) 'package:url_launcher/url_launcher.dart';
 import 'package:pmsbmibile3/style/pmsb_colors.dart';
 import 'package:pmsbmibile3/style/pmsb_styles.dart';
+import 'package:pmsbmibile3/widgets/round_image.dart';
 import 'administracao_home_page_bloc.dart';
 import 'package:pmsbmibile3/bootstrap.dart';
 
@@ -65,10 +67,7 @@ class _AdministracaoHomePageState extends State<AdministracaoHomePage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: <Widget>[
-                          // Expanded(
-                          //   flex: 10,
-                          //   child: Text('Lista dos usuarios ativos'),
-                          // ),
+                          // Botao de dowload pdf
                           Wrap(alignment: WrapAlignment.start, children: <
                               Widget>[
                             (snapshot.data?.relatorioPdfMakeModel?.pdfGerar !=
@@ -125,6 +124,7 @@ class _AdministracaoHomePageState extends State<AdministracaoHomePage> {
                           ]),
                         ],
                       ),
+                      // Lita de usuarios
                       Expanded(
                         flex: 10,
                         child: ListView(
@@ -156,69 +156,64 @@ class ItemListView extends StatelessWidget {
           Navigator.pushNamed(context, "/administracao/perfil",
               arguments: usuario.id);
         },
-        child: adminCard()
-        // Card(
-        //   child: Container(
-        //     padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-        //     child: Row(
-        //       crossAxisAlignment: CrossAxisAlignment.start,
-        //       children: <Widget>[
-        //         Expanded(
-        //           flex: 2,
-        //           child: _ImagemUnica(
-        //             fotoLocalPath: usuario?.foto?.localPath,
-        //             fotoUrl: usuario?.foto?.url,
-        //           ),
-        //         ),
-        //         Expanded(
-        //           flex: 5,
-        //           child: Container(
-        //             padding: EdgeInsets.only(left: 6),
-        //             child: Column(
-        //               crossAxisAlignment: CrossAxisAlignment.start,
-        //               children: <Widget>[
-        //                 Text("ID: ${usuario.id.substring(0, 5)}"),
-        //                 Text("Nome: ${usuario.nome}"),
-        //                 Text("Celular: ${usuario.celular}"),
-        //                 Text("Email: ${usuario.email}"),
-        //                 Text("Eixo: ${usuario.eixoIDAtual.nome}"),
-        //               ],
-        //             ),
-        //           ),
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // ),
-        );
+        child: adminCard(context));
   }
 
-  adminCard() {
+  double getWidgetSize(
+      {double medida, double webFullScream, double webHalf, double mobile}) {
+    return kIsWeb
+        ? (medida > 1000 ? medida * webFullScream : medida * webHalf)
+        : medida * mobile;
+  }
+
+  adminCard(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(
-            top: 6, bottom: 0), //distância entre os cards e o topo
+        padding: EdgeInsets.symmetric(
+            horizontal: getWidgetSize(
+          medida: MediaQuery.of(context).size.width,
+          mobile: 0.03,
+          webFullScream: 0.20,
+          webHalf: 0.05,
+        )
+            ),
         child: Container(
-          height: 140.0, //distância entre os cards
+          height: getWidgetSize(
+            medida: MediaQuery.of(context).size.width,
+            mobile: 0.40,
+            webFullScream: 0.12,
+            webHalf: 0.25,
+          ),
           child: Stack(
             children: <Widget>[
+              // LISTA DE INFORMACOES DO USUARIO
               Positioned(
-                left:
-                    50.0, //distância do card por trás da imagem para a esquerda
-                right: 4, //distância do card por trás da imagem para a direita
+                //distância do card por trás da imagem para a esquerda
+                left: 50.0,
+                //distância do card por trás da imagem para a direita
+                right: 4,
                 child: Container(
                   width: 290.0,
-                  height: 138.0, // espessura do card
+                  // espessura do card
+                  height: getWidgetSize(
+                    medida: MediaQuery.of(context).size.width,
+                    mobile: 0.38,
+                    webFullScream: 0.11,
+                    webHalf: 0.23,
+                  ),
                   child: Card(
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(17.0),
                     ),
                     color: PmsbColors.card,
                     child: Padding(
-                      padding: const EdgeInsets.only(
+                      padding: EdgeInsets.only(
                         top: 9.0,
-                        //bottom: 20.0,
-                        left:
-                            80.0, // distância do campo de texto para o da imagem
+                        left: getWidgetSize(
+                          medida: MediaQuery.of(context).size.width,
+                          mobile: 0.21,
+                          webFullScream: 0.11,
+                          webHalf: 0.20,
+                        ),
                       ),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -227,9 +222,10 @@ class ItemListView extends StatelessWidget {
                           Text(
                             "${usuario.nome}",
                             style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           admimRowLine("Celular", usuario.celular),
                           admimRowLine("E-mail:", usuario.email),
@@ -240,9 +236,26 @@ class ItemListView extends StatelessWidget {
                   ),
                 ),
               ),
+
+              //IMAGEM DO USUARIO
               Positioned(
-                top: 9.0, //altura da imagem de perfil
-                child: _ImagemAdminUnica(
+                //altura da imagem de perfil
+                top: 9.0,
+                child: RoundImage(
+                    corBorda: PmsbColors.cor_destaque,
+                    espesuraBorda: 5.0,
+                    heigth: getWidgetSize(
+                      medida: MediaQuery.of(context).size.width,
+                      mobile: 0.32,
+                      webFullScream: 0.10,
+                      webHalf: 0.20,
+                    ),
+                    width: getWidgetSize(
+                      medida: MediaQuery.of(context).size.width,
+                      mobile: 0.32,
+                      webFullScream: 0.10,
+                      webHalf: 0.20,
+                    ),
                     fotoLocalPath: usuario?.foto?.localPath,
                     fotoUrl: usuario?.foto?.url),
               ),
@@ -267,63 +280,171 @@ class ItemListView extends StatelessWidget {
   }
 }
 
-class _ImagemAdminUnica extends StatelessWidget {
-  final String fotoUrl;
-  final String fotoLocalPath;
+// adminCard(BuildContext context) {
+//   return Padding(
+//       padding: const EdgeInsets.only(
+//         top: 6,
+//         bottom: 0,
+//       ), //distância entre os cards e o topo
+//       child: Container(
+//         height: 140.0, //distância entre os cards
+//         child: Stack(
+//           children: <Widget>[
+//             // LISTA DE INFORMACOES DO USUARIO
+//             Positioned(
+//               //distância do card por trás da imagem para a esquerda
+//               left: 50.0,
+//               //distância do card por trás da imagem para a direita
+//               right: 4,
+//               child: Container(
+//                 width: 290.0,
+//                 // espessura do card
+//                 height: 138.0,
+//                 child: Card(
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(17.0),
+//                   ),
+//                   color: PmsbColors.card,
+//                   child: Padding(
+//                     padding: const EdgeInsets.only(
+//                       top: 9.0,
+//                       // distância do campo de texto para o da imagem
+//                       left: 80.0,
+//                     ),
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: <Widget>[
+//                         Text(
+//                           "${usuario.nome}",
+//                           style: TextStyle(
+//                             color: Colors.white,
+//                             fontSize: 20,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                         admimRowLine("Celular", usuario.celular),
+//                         admimRowLine("E-mail:", usuario.email),
+//                         admimRowLine("Eixo", usuario.eixoIDAtual.nome),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
 
-  const _ImagemAdminUnica({this.fotoUrl, this.fotoLocalPath});
+//             //IMAGEM DO USUARIO
+//             Positioned(
+//               //altura da imagem de perfil
+//               top: 9.0,
+//               child: RoundImage(
+//                   corBorda: PmsbColors.cor_destaque,
+//                   espesuraBorda: 5.0,
+//                   heigth: kIsWeb
+//                       ? MediaQuery.of(context).size.width * 0.10
+//                       : MediaQuery.of(context).size.width * 0.30,
+//                   width: kIsWeb
+//                       ? MediaQuery.of(context).size.width * 0.10
+//                       : MediaQuery.of(context).size.width * 0.30,
+//                   fotoLocalPath: usuario?.foto?.localPath,
+//                   fotoUrl: usuario?.foto?.url),
+//             ),
+//           ],
+//         ),
+//       ));
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    Widget foto;
-    final _width = MediaQuery.of(context).size.width;
+// class _ImagemAdminUnica extends StatelessWidget {
+//   final String fotoUrl;
+//   final String fotoLocalPath;
 
-    if (fotoUrl != null) {
-      foto = Container(
-        width: _width * 0.30, // espessura da imagem de perfil
-        height: _width * 0.30, // altura da imagem de perfil
-        decoration: BoxDecoration(
-          color: PmsbColors.card,
-          //borda ao redor da imagem de perfil
-          border: Border.all(
-            width: 5.0, //espessura da borda
-            color: PmsbColors.cor_destaque, // cor da borda
-          ),
-          shape: BoxShape.circle, //formato imagem
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage(fotoUrl), //chama url
-          ),
-        ),
-      );
-    } else {
-      String mensagem = (fotoUrl == null && fotoLocalPath == null)
-          ? "Sem imagem."
-          : "Não enviada.";
+//   const _ImagemAdminUnica({this.fotoUrl, this.fotoLocalPath});
 
-      foto = Stack(
-        children: <Widget>[
-          Container(
-            width: _width * 0.30, // espessura da imagem de perfil
-            height: _width * 0.30, // altura da imagem de perfil
-            decoration: BoxDecoration(
-              color: PmsbColors.card,
-              //borda ao redor da imagem de perfil
-              border: Border.all(
-                width: 5.0, //espessura da borda
-                color: PmsbColors.cor_destaque, // cor da borda
-              ),
-              shape: BoxShape.circle, //formato imagem
-            ),
-          ),
-          Positioned(
-              top: _width * 0.13, left: _width * 0.03, child: Text(mensagem)),
-        ],
-      );
-    }
-    return foto;
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget foto;
+//     final _width = MediaQuery.of(context).size.width;
+
+//     if (fotoUrl != null) {
+//       foto = Container(
+//         width: _width * 0.30, // espessura da imagem de perfil
+//         height: _width * 0.30, // altura da imagem de perfil
+//         decoration: BoxDecoration(
+//           color: PmsbColors.card,
+//           //borda ao redor da imagem de perfil
+//           border: Border.all(
+//             width: 5.0, //espessura da borda
+//             color: PmsbColors.cor_destaque, // cor da borda
+//           ),
+//           shape: BoxShape.circle, //formato imagem
+//           image: DecorationImage(
+//             fit: BoxFit.cover,
+//             image: NetworkImage(fotoUrl), //chama url
+//           ),
+//         ),
+//       );
+//     } else {
+//       String mensagem = (fotoUrl == null && fotoLocalPath == null)
+//           ? "Sem imagem."
+//           : "Não enviada.";
+
+//       foto = Stack(
+//         children: <Widget>[
+//           Container(
+//             width: _width * 0.30, // espessura da imagem de perfil
+//             height: _width * 0.30, // altura da imagem de perfil
+//             decoration: BoxDecoration(
+//               color: PmsbColors.card,
+//               //borda ao redor da imagem de perfil
+//               border: Border.all(
+//                 width: 5.0, //espessura da borda
+//                 color: PmsbColors.cor_destaque, // cor da borda
+//               ),
+//               shape: BoxShape.circle, //formato imagem
+//             ),
+//           ),
+//           Positioned(
+//               top: _width * 0.13, left: _width * 0.03, child: Text(mensagem)),
+//         ],
+//       );
+//     }
+//     return foto;
+//   }
+// }
+
+// Card(
+//   child: Container(
+//     padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+//     child: Row(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: <Widget>[
+//         Expanded(
+//           flex: 2,
+//           child: _ImagemUnica(
+//             fotoLocalPath: usuario?.foto?.localPath,
+//             fotoUrl: usuario?.foto?.url,
+//           ),
+//         ),
+//         Expanded(
+//           flex: 5,
+//           child: Container(
+//             padding: EdgeInsets.only(left: 6),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: <Widget>[
+//                 Text("ID: ${usuario.id.substring(0, 5)}"),
+//                 Text("Nome: ${usuario.nome}"),
+//                 Text("Celular: ${usuario.celular}"),
+//                 Text("Email: ${usuario.email}"),
+//                 Text("Eixo: ${usuario.eixoIDAtual.nome}"),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ],
+//     ),
+//   ),
+// ),
 
 // class _ImagemUnica extends StatelessWidget {
 //   final String fotoUrl;
