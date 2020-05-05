309d55c31040de0ed7795275bed01fe62a9ac45d
Élenn Milhomem <elenndypaulla@hotmail.com>
2020-05-05 13:00:52 -0300

    #440 Começo da implementação da UI de controle de tarefas

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\natab\Desktop\Projetos\pmsbmobile3"
+export "FLUTTER_ROOT=D:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\teste\Documents\GitHub\pmsbmobile3"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\src\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=1"
--- lib/pages/controle/controle_tarefa_list_page.dart
+++ lib/pages/controle/controle_tarefa_list_page.dart
@@ -8,6 +8,7 @@ import 'package:pmsbmibile3/naosuportato/url_launcher.dart'
     if (dart.library.io) 'package:url_launcher/url_launcher.dart';
 import 'package:pmsbmibile3/style/pmsb_colors.dart';
 import 'package:pmsbmibile3/style/pmsb_styles.dart';
+import 'package:drag_list/drag_list.dart';
 
 import 'controle_tarefa_list_bloc.dart';
 
@@ -22,6 +23,11 @@ class ControleTarefaListPage extends StatefulWidget {
 
 class _ControleTarefaListPageState extends State<ControleTarefaListPage> {
   ControleTarefaListBloc bloc;
+  List<String> cards = ["Aberta", "Feita"];
+  List<List<String>> childres = [
+    ["Aberta 1", "Aberta 2"],
+    ["Feita 1", "Feita 2"],
+  ];
 
   @override
   void initState() {
@@ -36,7 +42,385 @@ class _ControleTarefaListPageState extends State<ControleTarefaListPage> {
     bloc.dispose();
   }
 
-  _bodyDestinatario(context) {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultScaffold(
+      backToRootPage: true,
+      title: Text("Controle de Tarefas"),
+      body: _buildBody(),
+    );
+  }
+
+  TextEditingController _cardTextController = TextEditingController();
+  TextEditingController _taskTextController = TextEditingController();
+
+  _showAddCard() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return Dialog(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Add Card",
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(hintText: "Card Title"),
+                    controller: _cardTextController,
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Center(
+                  child: RaisedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      _addCard(_cardTextController.text.trim());
+                    },
+                    child: Text("Add Card"),
+                  ),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  _addCard(String text) {
+    cards.add(text);
+    childres.add([]);
+    _cardTextController.text = "";
+    setState(() {});
+  }
+
+  _showAddCardTask(int index) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return Dialog(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Add Card task",
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(hintText: "Task Title"),
+                    controller: _taskTextController,
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Center(
+                  child: RaisedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      _addCardTask(index, _taskTextController.text.trim());
+                    },
+                    child: Text("Add Task"),
+                  ),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  _addCardTask(int index, String text) {
+    childres[index].add(text);
+    _taskTextController.text = "";
+    setState(() {});
+  }
+
+  _handleReOrder(int oldIndex, int newIndex, int index) {
+    var oldValue = childres[index][oldIndex];
+    childres[index][oldIndex] = childres[index][newIndex];
+    childres[index][newIndex] = oldValue;
+    setState(() {});
+  }
+
+  _buildBody() {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: cards.length + 1,
+      itemBuilder: (context, index) {
+        if (index == cards.length)
+          return _buildAddCardWidget(context);
+        else
+          return _buildCard(context, index);
+      },
+    );
+  }
+
+  Widget _buildAddCardWidget(context) {
+    return Column(
+      children: <Widget>[
+        InkWell(
+          onTap: () {
+            _showAddCard();
+          },
+          child: Container(
+            width: 300.0,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 8,
+                    offset: Offset(0, 0),
+                    color: Color.fromRGBO(127, 140, 141, 0.5),
+                    spreadRadius: 2)
+              ],
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.white,
+            ),
+            margin: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.add,
+                ),
+                SizedBox(
+                  width: 16.0,
+                ),
+                Text("Add Card"),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAddCardTaskWidget(context, index) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: InkWell(
+        onTap: () {
+          _showAddCardTask(index);
+        },
+        child: Row(
+          children: <Widget>[
+            Icon(
+              Icons.add,
+            ),
+            SizedBox(
+              width: 16.0,
+            ),
+            Text("Add Card Task"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(BuildContext context, int index) {
+    // return Container(
+    //         width: 300.0,
+    //   child: ,
+    // );
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 0.300,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 8,
+                    offset: Offset(0, 0),
+                    color: Color.fromRGBO(127, 140, 141, 0.5),
+                    spreadRadius: 1)
+              ],
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.white,
+            ),
+            margin: const EdgeInsets.all(16.0),
+            height: MediaQuery.of(context).size.height * 0.8,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    cards[index],
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                /*SingleChildScrollView(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    child: DragAndDropList<String>(
+                      childres[index],
+                      itemBuilder: (BuildContext context, item) {
+                        return _buildCardTask(
+                            index, childres[index].indexOf(item));
+                      },
+                      onDragFinish: (oldIndex, newIndex) {
+                        _handleReOrder(oldIndex, newIndex, index);
+                      },
+                      canBeDraggedTo: (one, two) => true,
+                      dragElevation: 8.0,
+                    ),
+                  ),
+               */ // ),
+              ],
+            ),
+          ),
+          Positioned.fill(
+            child: DragTarget<dynamic>(
+              onWillAccept: (data) {
+                print(data);
+                return true;
+              },
+              onLeave: (data) {},
+              onAccept: (data) {
+                if (data['from'] == index) {
+                  return;
+                }
+                childres[data['from']].remove(data['string']);
+                childres[index].add(data['string']);
+                print(data);
+                setState(() {});
+              },
+              builder: (context, accept, reject) {
+                print("--- > $accept");
+                print(reject);
+                return Container();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildCardTask(int index, int innerIndex) {
+    return Container(
+      width: 300.0,
+      margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Draggable<dynamic>(
+        feedback: Material(
+          elevation: 5.0,
+          child: Container(
+            width: 284.0,
+            padding: const EdgeInsets.all(16.0),
+            color: Colors.greenAccent,
+            child: Text(childres[index][innerIndex]),
+          ),
+        ),
+        childWhenDragging: Container(),
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          color: Colors.greenAccent,
+          child: Text(childres[index][innerIndex]),
+        ),
+        data: {"from": index, "string": childres[index][innerIndex]},
+      ),
+    );
+  }
+}
+
+/// Selecao de setor para duplicar a tarefa
+class SetorListaModalSelect extends StatefulWidget {
+  final ControleTarefaListBloc bloc;
+  final ControleTarefaModel tarefaID;
+
+  const SetorListaModalSelect(this.bloc, this.tarefaID);
+
+  @override
+  _SetorListaModalSelectState createState() =>
+      _SetorListaModalSelectState(this.bloc);
+}
+
+class _SetorListaModalSelectState extends State<SetorListaModalSelect> {
+  final ControleTarefaListBloc bloc;
+
+  _SetorListaModalSelectState(this.bloc);
+
+  Widget _listaSetor() {
+    return StreamBuilder<ControleTarefaListBlocState>(
+      stream: bloc.stateStream,
+      builder: (BuildContext context,
+          AsyncSnapshot<ControleTarefaListBlocState> snapshot) {
+        if (snapshot.hasError)
+          return Center(
+            child: Text("Erro. Informe ao administrador do aplicativo"),
+          );
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.data.tarefaDuplicadaNoSetorMap.isEmpty) {
+          return Center(
+            child: Text("Nenhum setor encontrado."),
+          );
+        }
+
+        var lista = List<Widget>();
+        for (var setor in snapshot.data.tarefaDuplicadaNoSetorMap.entries) {
+          lista.add(_cardBuild(context, setor.value));
+        }
+
+        return ListView(
+          children: lista,
+        );
+      },
+    );
+  }
+
+  Widget _cardBuild(
+      BuildContext context, TarefaDuplicadaNoSetor tarefaDuplicadaNoSetor) {
+    return ListTile(
+      title: Text('${tarefaDuplicadaNoSetor.setorCensitarioID.nome}'),
+      selected: tarefaDuplicadaNoSetor.duplicado,
+      trailing: tarefaDuplicadaNoSetor.duplicado
+          ? Icon(Icons.check)
+          : IconButton(
+              icon: Icon(Icons.clear),
+              onPressed: () {
+                bloc.eventSink(DuplicarTarefaEvent(
+                    setorID: tarefaDuplicadaNoSetor.setorCensitarioID,
+                    tarefaID: widget.tarefaID));
+                Navigator.pop(context);
+              },
+            ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Escolha um setor"),
+      ),
+      body: _listaSetor(),
+    );
+  }
+}
+/* _bodyDestinatario(context) {
     return StreamBuilder<ControleTarefaListBlocState>(
       stream: bloc.stateStream,
       builder: (BuildContext context,
@@ -385,84 +769,4 @@ class _ControleTarefaListPageState extends State<ControleTarefaListPage> {
         body: _body(context),
       ),
     );
-  }
-}
-
-/// Selecao de setor para duplicar a tarefa
-class SetorListaModalSelect extends StatefulWidget {
-  final ControleTarefaListBloc bloc;
-  final ControleTarefaModel tarefaID;
-
-  const SetorListaModalSelect(this.bloc, this.tarefaID);
-
-  @override
-  _SetorListaModalSelectState createState() =>
-      _SetorListaModalSelectState(this.bloc);
-}
-
-class _SetorListaModalSelectState extends State<SetorListaModalSelect> {
-  final ControleTarefaListBloc bloc;
-
-  _SetorListaModalSelectState(this.bloc);
-
-  Widget _listaSetor() {
-    return StreamBuilder<ControleTarefaListBlocState>(
-      stream: bloc.stateStream,
-      builder: (BuildContext context,
-          AsyncSnapshot<ControleTarefaListBlocState> snapshot) {
-        if (snapshot.hasError)
-          return Center(
-            child: Text("Erro. Informe ao administrador do aplicativo"),
-          );
-        if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        if (snapshot.data.tarefaDuplicadaNoSetorMap.isEmpty) {
-          return Center(
-            child: Text("Nenhum setor encontrado."),
-          );
-        }
-
-        var lista = List<Widget>();
-        for (var setor in snapshot.data.tarefaDuplicadaNoSetorMap.entries) {
-          lista.add(_cardBuild(context, setor.value));
-        }
-
-        return ListView(
-          children: lista,
-        );
-      },
-    );
-  }
-
-  Widget _cardBuild(
-      BuildContext context, TarefaDuplicadaNoSetor tarefaDuplicadaNoSetor) {
-    return ListTile(
-      title: Text('${tarefaDuplicadaNoSetor.setorCensitarioID.nome}'),
-      selected: tarefaDuplicadaNoSetor.duplicado,
-      trailing: tarefaDuplicadaNoSetor.duplicado
-          ? Icon(Icons.check)
-          : IconButton(
-              icon: Icon(Icons.clear),
-              onPressed: () {
-                bloc.eventSink(DuplicarTarefaEvent(
-                    setorID: tarefaDuplicadaNoSetor.setorCensitarioID,
-                    tarefaID: widget.tarefaID));
-                Navigator.pop(context);
-              },
-            ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Escolha um setor"),
-      ),
-      body: _listaSetor(),
-    );
-  }
-}
+  */ //}
