369df04da2fb1a48f097c347d46954e304286000
Élenn Milhomem <elenndypaulla@hotmail.com>
2020-06-09 18:02:53 -0300

    Design da tela do quadro de tarefas do controle

--- lib/pages/controle/controle_home_page.dart
+++ lib/pages/controle/controle_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pmsbmibile3/components/default_scaffold.dart';
+import 'package:pmsbmibile3/models/models_controle/acao_model.dart';
+import 'package:pmsbmibile3/models/models_controle/etiqueta_model.dart';
 import 'package:pmsbmibile3/models/models_controle/tarefa_model.dart';
 import 'package:pmsbmibile3/state/auth_bloc.dart';
 import 'package:pmsbmibile3/style/pmsb_colors.dart';
@@ -17,9 +19,22 @@ class ControleTarefaHomePage extends StatefulWidget {
 }
 
 class _ControleTarefaHomePageState extends State<ControleTarefaHomePage> {
+  static List<Acao> listaAcao = [
+    Acao(titulo: "", status: true),
+    Acao(titulo: "", status: false),
+  ];
+
+  static List<Etiqueta> listaEtiquetas = [
+    Etiqueta(cor: 0xFF20BBE8, titulo: "Wip"),
+    Etiqueta(cor: 0xFFFF5733, titulo: "Help"),
+    Etiqueta(cor: 0xFF8FD619 , titulo: "Working"),
+  ];
+
   TarefaModel cardModel01 = new TarefaModel(
     descricaoAtividade: "Descrição teste",
     tituloAtividade: "Título do Card 01",
+    acoes: listaAcao,
+    etiquetas: listaEtiquetas,
    // publico: true,
   );
 
--- lib/widgets/tarefa_card_widget.dart
+++ lib/widgets/tarefa_card_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pmsbmibile3/models/models_controle/etiqueta_model.dart';
 import 'package:pmsbmibile3/models/models_controle/tarefa_model.dart';
+import 'package:pmsbmibile3/style/pmsb_colors.dart';
+import 'package:pmsbmibile3/widgets/etiqueta_card_widget.dart';
 
 class TarefaCardWidget extends StatelessWidget {
   final Color cor;
@@ -14,14 +17,39 @@ class TarefaCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: this.cor,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: PmsbColors.card,
+        border: Border(),
+      ),
+      //color: this.cor,
       height: this.altura,
       width: this.largura,
-      child: ListTile(
-        //trailing: Icon(this.tarefa.publico ? Icons.lock_open : Icons.lock),
-        title: Text("${this.tarefa.tituloAtividade}"),
-        subtitle: Text("Descrição: ${this.tarefa.descricaoAtividade}"),
+      child: Column(
+        children: [
+          ListTile(
+            //trailing: Icon(this.tarefa.publico ? Icons.lock_open : Icons.lock),
+            title: Text("${this.tarefa.tituloAtividade}"),
+            subtitle: Text(
+                "Ações:${tarefa.getQuantAcoesFeitas()}/${tarefa.acoes.length}"),
+            //subtitle: Text("Descrição: ${this.tarefa.descricaoAtividade}"),
+          ),
+          Wrap(
+            crossAxisAlignment: WrapCrossAlignment.start,
+            children: gerarListaEtiquetas(),
+          )
+        ],
       ),
     );
+  }
+
+  List<Widget> gerarListaEtiquetas() {
+    List<Widget> listaEtiqueta = List<Widget>();
+    for (Etiqueta etq in tarefa.etiquetas) {
+      listaEtiqueta.add(EtiquetaCardWidget(
+        etiqueta: etq,
+      ));
+    }
+    return listaEtiqueta;
   }
 }
